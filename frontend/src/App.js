2eb547a897b36505eb73da811dda52a8b04dc2c6
Tarikul islam <tarikul.cse5.bu@gmail.com>
2022-05-09 23:36:04 +0600

    create-react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => {
+	return (
+		<div>
+			<header>
+				<a href='/'>amazon</a>
+			</header>
+      <main>
+      product list</main>
+		</div>
+	);
+};
+
+export default App;
